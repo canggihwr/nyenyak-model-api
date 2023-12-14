3af9de0b8dec995bb25a7471748a4c942c210040
canggihwr <canggihrinaldi8@gmail.com>
2023-12-14 21:53:19 +0800

    update env port

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def predict_sleep_disorder():
     return jsonify(content)
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
